3db539d30b936e0c3267a7b4cc340c4becaf11ee
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-08-31 18:33:10 +0300

    Edited project create blade

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,48 +2,48 @@
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+        <b>Version</b> 3.2.0
     </div>
-  </footer>
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+</aside>
+<!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="{{asset('plugins/chart.js')}}/Chart.min.js')}}"></script>
+<script src="{{ asset('plugins/chart.js') }}/Chart.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="{{asset('plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{ asset('plugins/sparklines/sparkline.js') }}"></script>
 <!-- JQVMap -->
-<script src="{{asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="{{asset('plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('dist/js/adminlte.js')}}"></script>
+<script src="{{ asset('dist/js/adminlte.js') }}"></script>
 
 <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <script src="{{ asset('plugins/codemirror/codemirror.js') }}"></script>
@@ -51,18 +51,17 @@
 <script src="{{ asset('plugins/codemirror/mode/xml/xml.js') }}"></script>
 <script src="{{ asset('plugins/codemirror/mode/htmlmixed/htmlmixed.js') }}"></script>
 <script>
-  $(function () {
-    // Summernote
-    $('#summernote').summernote()
+    $(function() {
+        // Summernote
+        $('#summernote, #summernote_en, #summernote_ar').summernote()
 
-    // CodeMirror
-    CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
-      mode: "htmlmixed",
-      theme: "monokai"
-    });
-  })
+        // CodeMirror
+        CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+            mode: "htmlmixed",
+            theme: "monokai"
+        });
+    })
 </script>
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
-
+<script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -1,95 +1,111 @@
 @extends('admin.index')
-@section('admin') 
+@section('admin')
+    <div class="col-md-12">
 
-
-
-
-<div class="col-md-12">
-
-<div class="card card-primary">
-              <div class="card-header">
+        <div class="card card-primary">
+            <div class="card-header">
                 <h3 class="card-title">Create New Project</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form action="{{route('project.store')}}" method="post" enctype="multipart/form-data">
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form action="{{ route('project.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
                 <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Project Name</label>
-                    <input type="text" name="name" class="form-control" id="exampleInputEmail1">
-                  </div>
-              
-
-                  <div class="card-body">
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Project link</label>
-                      <input type="text" name="link" class="form-control" id="exampleInputEmail1">
+                        <label for="exampleInputEmail1">Project Name in English</label>
+                        <input type="text" name="name_en" class="form-control" id="exampleInputEmail1">
+                        @error('name_en')
+                            <div class="text text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
-                
-
-                  <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Project description</h1>
-          </div>
-          
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card card-outline card-info">
-            <div class="card-header">
-            
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <textarea id="summernote" name="content">
-                Place <em>some</em> <u>text</u> <strong>here</strong>
-              </textarea>
-            </div>
-         
-          </div>
-        </div>
-        <!-- /.col-->
-      </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" name="image">
-                      
-                      </div>
-                    
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Project Name in Arabic</label>
+                        <input type="text" name="name_ar" class="form-control" id="exampleInputEmail1">
+                        @error('name_ar')
+                            <div class="text text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
-                  </div>
-              
-                </div>
-                <!-- /.card-body -->
-
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </form>
-            </div>
 
 
-</div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Project link</label>
+                            <input type="text" name="link" class="form-control" id="exampleInputEmail1">
+                            @error('link')
+                                <div class="text text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
 
 
+                        <section class="content-header">
+                            <div class="container-fluid">
+                                <div class="row mb-2">
+                                    <div class="col-sm-6">
+                                        <h1>Project description</h1>
+                                    </div>
+
+                                </div>
+                            </div><!-- /.container-fluid -->
+                        </section>
+
+                        <!-- Main content -->
+                        <section class="content">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card card-outline card-info">
+                                        <div class="card-header">
+                                            <h3>English description</h3>
+                                        </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <textarea id="summernote_en" name="content_en">
+                                            </textarea>
+                                            @error('content_en')
+                                                <div class="text text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="card card-outline card-info">
+                                        <div class="card-header">
+                                            <h3>Arabic description</h3>
+                                        </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <textarea id="summernote_ar" name="content_ar">
+                                            </textarea>
+                                            @error('content_ar')
+                                                <div class="text text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- /.col-->
+                        </section>
+                        <div class="form-group">
+                            <label for="exampleInputFile">File input</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" name="image">
+                                    @error('image')
+                                        <div class="text text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+            </form>
+        </div>
 
 
-
-
-
-
-
-
-
+    </div>
 @endsection
